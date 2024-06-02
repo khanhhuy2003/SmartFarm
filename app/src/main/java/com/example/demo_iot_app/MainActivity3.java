c89f6fe218686861b49eb01b6c03037be3ed8786
HUYTRAN\DELL <huyhoa0502@gmail.com>
2024-06-02 17:01:24 +0700

    adding temp humid presentation

--- app/src/main/java/com/example/demo_iot_app/MainActivity3.java
+++ app/src/main/java/com/example/demo_iot_app/MainActivity3.java
@@ -3,13 +3,73 @@ package com.example.demo_iot_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 
-// for display temperature
+import java.nio.charset.Charset;
+
+//For display temperature
 public class MainActivity3 extends AppCompatActivity {
+
+    MQTTHelper mqttHelper;
+    TextView temp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3);
+        setContentView(R.layout.activity_main4);
+        startMQTT();
+        temp = findViewById(R.id.temp);
     }
+
+
+    public void sendDataMQTT(String topic, String value){
+        MqttMessage msg = new MqttMessage();
+        msg.setId(1234);
+        msg.setQos(0);
+        msg.setRetained(false);
+
+        byte[] b = value.getBytes(Charset.forName("UTF-8"));
+        msg.setPayload(b);
+
+        try {
+            mqttHelper.mqttAndroidClient.publish(topic, msg);
+        }catch (MqttException e){
+
+        }
+    }
+
+    public void startMQTT(){
+        mqttHelper = new MQTTHelper(this);
+        mqttHelper.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean reconnect, String serverURI) {
+
+            }
+
+            @Override
+            public void connectionLost(Throwable cause) {
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                Log.d("TEST", topic + "***" +message.toString());
+                if(topic.contains("huytran1305/feeds/assignment.temperature")) {
+                    temp.setText(message.toString() + "%");
+                }
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+        });
+    }
+
 }
